f329287b1b4d628a6180d3b2d770a694a93983e4
Md Zahid Hasan <jahid1234.jh@gmail.com>
2025-05-21 12:09:50 +0600

    jh

--- resources/js/views/dashboard/Index.vue
+++ resources/js/views/dashboard/Index.vue
@@ -5,7 +5,7 @@
             <div class="col-sm-6">
                 <h4 class="page-title">Dashboard</h4>
                 <ol class="breadcrumb">
-                    <li class="breadcrumb-item active">Welcome to LA Monitoring Admin Dashboard</li>
+                    <li class="breadcrumb-item active">Welcome to E Verification Admin Dashboard</li>
                 </ol>
             </div>
             <div class="col-sm-6">
@@ -16,76 +16,6 @@
                             <div class="dropdown-divider"></div>
                             <a class="dropdown-item" href="#">Separated link</a>
                         </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-xl-3 col-md-6">
-            <div class="card mini-stat bg-primary text-white">
-                <div class="card-body">
-                    <div class="mb-4">
-                        <div class="float-left mini-stat-img mr-4"><img src="assets/images/services-icon/01.png" alt="" /></div>
-                        <h5 class="font-16 text-uppercase mt-0 text-white-50">Total Farms</h5>
-                        <h4 class="font-500">1,685 <i class="mdi mdi-arrow-up text-success ml-2"></i></h4>
-                    </div>
-                    <div class="pt-2">
-                        <div class="float-right">
-                            <a href="#" class="text-white-50"><i class="mdi mdi-arrow-right h5"></i></a>
-                        </div>
-                        <p class="text-white-50 mb-0">Since last month</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="col-xl-3 col-md-6">
-            <div class="card mini-stat bg-primary text-white">
-                <div class="card-body">
-                    <div class="mb-4">
-                        <div class="float-left mini-stat-img mr-4"><img src="assets/images/services-icon/02.png" alt="" /></div>
-                        <h5 class="font-16 text-uppercase mt-0 text-white-50">Total Breeding</h5>
-                        <h4 class="font-500">52,368 <i class="mdi mdi-arrow-down text-danger ml-2"></i></h4>
-                    </div>
-                    <div class="pt-2">
-                        <div class="float-right">
-                            <a href="#" class="text-white-50"><i class="mdi mdi-arrow-right h5"></i></a>
-                        </div>
-                        <p class="text-white-50 mb-0">Since last month</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="col-xl-3 col-md-6">
-            <div class="card mini-stat bg-primary text-white">
-                <div class="card-body">
-                    <div class="mb-4">
-                        <div class="float-left mini-stat-img mr-4"><img src="assets/images/services-icon/03.png" alt="" /></div>
-                        <h5 class="font-16 text-uppercase mt-0 text-white-50">Total LA</h5>
-                        <h4 class="font-500">15.8 <i class="mdi mdi-arrow-up text-success ml-2"></i></h4>
-                    </div>
-                    <div class="pt-2">
-                        <div class="float-right">
-                            <a href="#" class="text-white-50"><i class="mdi mdi-arrow-right h5"></i></a>
-                        </div>
-                        <p class="text-white-50 mb-0">Since last month</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="col-xl-3 col-md-6">
-            <div class="card mini-stat bg-primary text-white">
-                <div class="card-body">
-                    <div class="mb-4">
-                        <div class="float-left mini-stat-img mr-4"><img src="assets/images/services-icon/04.png" alt="" /></div>
-                        <h5 class="font-16 text-uppercase mt-0 text-white-50">Total Achieved</h5>
-                        <h4 class="font-500">2436 <i class="mdi mdi-arrow-up text-success ml-2"></i></h4>
-                    </div>
-                    <div class="pt-2">
-                        <div class="float-right">
-                            <a href="#" class="text-white-50"><i class="mdi mdi-arrow-right h5"></i></a>
-                        </div>
-                        <p class="text-white-50 mb-0">Since last month</p>
                     </div>
                 </div>
             </div>
